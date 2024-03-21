684fa46f4814e2f144e59823b27c46ce36fc245e
Misa <ravololonantenainamisa7@gmail.com>
2024-03-21 10:36:48 +0300

    second commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import { DashboardComponent } from '../app/dashboard/dashboard/dashboard.component';
+import { InfoComponent } from '../app/dashboard/info/info.component';
+import { UserComponent } from '../app/dashboard/user/user.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    DashboardComponent,
+    InfoComponent,
+    UserComponent
   ],
   imports: [
     BrowserModule,
